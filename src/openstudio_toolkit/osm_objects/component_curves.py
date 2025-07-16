63b9faa44942dd3e98bf7df93cde4633cacdd090
Roberto Ruiz Flores <roruiz@gmail.com>
2025-07-16 00:37:04 -0400

    quadratic curves added in src/openstudio_toolkit/osm_objects/component_curves

--- src/openstudio_toolkit/osm_objects/component_curves.py
+++ src/openstudio_toolkit/osm_objects/component_curves.py
@@ -368,3 +368,110 @@ def get_all_curve_exponent_objects_as_dataframe(osm_model: openstudio.model.Mode
         f"The OSM model contains {all_objects_df.shape[0]} curve exponent objects.")
 
     return all_objects_df
+
+
+# --------------------------------------------------
+#  ***** OS:Curve:Quadratic ************************
+# --------------------------------------------------
+
+
+def get_curve_quadratic_object_as_dict(osm_model: openstudio.model.Model, handle: str = None, name: str = None) -> dict:
+    """
+    Gets a specified OS:Curve:Quadratic object from the OpenStudio model by either handle or name and return its attributes as a dictionary.
+
+    Parameters:
+    - osm_model (openstudio.model.Model): The OpenStudio Model object.
+    - handle (str, optional): The handle of the object to get.
+    - name (str, optional): The name of the object to get.
+
+    Returns:
+    - dict: Dictionary containing information about the specified object.
+    """
+    if handle is not None and name is not None:
+        raise ValueError(
+            "Only one of 'handle' or 'name' should be provided.")
+    if handle is None and name is None:
+        raise ValueError(
+            "Either 'handle' or 'name' must be provided.")
+
+    # Find the object by handle or name
+    if handle is not None:
+        osm_object = osm_model.getCurveQuadratic(handle)
+        if osm_object is None:
+            print(f"No object found with the handle: {handle}")
+            return {}
+
+    elif name is not None:
+        osm_object = osm_model.getCurveQuadraticByName(name)
+        if not osm_object:
+            print(f"No object found with the name: {name}")
+            return {}
+
+    target_object = osm_object.get()
+
+    # Define attributes to retrieve in a dictionary
+    object_dict = {'Handle': str(target_object.handle()), 
+                   'Name': target_object.nameString(), 
+                   'Coefficient1 Constant': target_object.coefficient1Constant(), 
+                   'Coefficient2 x': target_object.coefficient2x(), 
+                   'Coefficient3 x**2': target_object.coefficient3xPOW2(), 
+                   'Minimum Value of x': target_object.minimumValueofx(), 
+                   'Maximum Value of x': target_object.maximumValueofx()}
+
+    return object_dict
+
+def get_all_curve_quadratic_objects_as_dicts(osm_model: openstudio.model.Model) -> list[dict]:
+    """
+    Gets all OS:Curve:Quadratic objects from the OpenStudio model and return their attributes as a list of dictionaries.
+
+    Parameters:
+    - osm_model (openstudio.model.Model): The OpenStudio Model object.
+
+    Returns:
+    - list[dict]: A list of dictionaries, each containing information about a curve quadratic object.
+    """
+
+    # Get all spaces in the OpenStudio model.
+    all_objects = osm_model.getCurveQuadratics()
+
+    all_objects_dicts = []
+
+    for target_object in all_objects:
+        space_handle = str(target_object.handle())
+        object_dict = get_curve_quadratic_object_as_dict(osm_model, space_handle)
+        all_objects_dicts.append(object_dict)
+
+    return all_objects_dicts
+
+
+def get_all_curve_quadratic_objects_as_dataframe(osm_model: openstudio.model.Model) -> pd.DataFrame:
+    """
+    Gets all curve quadratic objects from the OpenStudio model and return their attributes as a pandas DataFrame.
+
+    Parameters:
+    - osm_model (openstudio.model.Model): The OpenStudio Model object.
+
+    Returns:
+    - pd.DataFrame: DataFrame containing information about all curve quadratic objects.
+    """
+
+    all_objects_dicts = get_all_curve_quadratic_objects_as_dicts(osm_model)
+
+    # Define the columns for the DataFrame
+    columns = ['Handle', 'Name', 'Coefficient1 Constant', 'Coefficient2 x', 'Coefficient3 x**2', 'Minimum Value of x', 'Maximum Value of x']
+
+    # If all_objects_dicts is None or empty, create an empty DataFrame with the defined columns
+    if not all_objects_dicts:
+        all_objects_df = pd.DataFrame(columns=columns)
+    else:
+        # Create a DataFrame of all curve quadratic objects.
+        all_objects_df = pd.DataFrame(all_objects_dicts)
+
+    # Sort the DataFrame alphabetically by the Name column and reset indexes
+    all_objects_df = all_objects_df.sort_values(
+        by='Name', ascending=True, na_position='first').reset_index(drop=True)
+
+    print(
+        f"The OSM model contains {all_objects_df.shape[0]} curve quadratic objects.")
+
+    return all_objects_df
